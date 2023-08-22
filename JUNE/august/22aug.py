13d9c0eb0ccefec857b45688a2bbdeb7294452ff
ANURAG-R-NAIK <111006990+ANURAG-R-NAIK@users.noreply.github.com>
2023-08-22 11:08:13 +0530

    22aug

---
+++ JUNE/august/22aug.py
@@ -0,0 +1,65 @@
+
+# s = input('enter')
+# from collections import Counter
+# def isIsogram(s):
+#         k = Counter(s)
+#         print(k)
+#         for i, j in k.items():
+#             if j > 1:
+#                 return 0
+#         return 1
+    
+# print(isIsogram(s))
+
+# OOPS STARTED FRIM HERE
+
+# class dog:
+#     attr1 = "mammal"
+#     attr2 = "dog"
+    
+#     def fun(self):
+#         print("I'm a", self.attr1)
+#         print("I'm a", self.attr2)  
+        
+# roger = dog()
+
+# print(roger.attr1)
+# roger.fun()
+#-------------------------
+
+# class person:
+    
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+        
+#     def __str__(self):
+#         return f"hello my name is {self.name} and my age is {self.age}"
+    
+# p = person('anurag', 21)
+# print(p)
+#----------------
+ 
+# class dog:
+    
+#     animal = 'dog'
+    
+#     def __init__(self, breed, color):
+#         self.breed = breed
+#         self.color = color
+        
+# roger = dog('pug', 'brown')
+# buzo = dog('bulldog', 'black')
+
+# print(roger.animal)
+# print(roger.breed)
+# print(roger.color)
+
+
+# print(buzo.animal)
+# print(buzo.breed)
+# print(buzo.color)
+        
+        
+    
+    
